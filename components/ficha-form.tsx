562e63e33f70442dad31c93316583a7727eedac9
São Jose <eazytech.fh@gmail.com>
2026-04-06 13:19:54 -0300

    Fix missing form button import

--- components/ficha-form.tsx
+++ components/ficha-form.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect, useRef } from "react"
+import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Input } from "@/components/ui/input"
