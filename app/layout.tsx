b87e994a9bae26e9f8657edc5ed284f1b7323159
Herbert Farias <herbert.fariasss@gmail.com>
2026-08-05 10:05:16 -0300

    mudança de log

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,11 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
+      { url: '/favicon-car-32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-car-512.png', sizes: '512x512', type: 'image/png' },
     ],
-    apple: '/apple-icon.png',
+    shortcut: '/favicon-car-32.png',
+    apple: { url: '/favicon-car-180.png', sizes: '180x180', type: 'image/png' },
   },
 }
 
